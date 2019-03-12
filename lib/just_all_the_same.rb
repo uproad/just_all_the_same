db13ec2753a1a4c2377da549fb64780bfec11ac2
uproad krowd <7349115+uproad@users.noreply.github.com>
2019-03-12 16:43:13 +0900

    [add] mock method all_nil?

--- lib/just_all_the_same.rb
+++ lib/just_all_the_same.rb
@@ -1,6 +1,9 @@
 require "just_all_the_same/version"
 
 module JustAllTheSame
-  class Error < StandardError; end
-  # Your code goes here...
+  class ::Array
+    def all_nil?
+      all?(nil)
+    end
+  end
 end
