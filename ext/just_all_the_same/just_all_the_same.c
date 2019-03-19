a1d2ea86edddb6ceba9a0e5ecbf706992117ecf8
uproad krowd <7349115+uproad@users.noreply.github.com>
2019-03-19 20:27:49 +0900

    [add] all_nil_p implementation

--- ext/just_all_the_same/just_all_the_same.c
+++ ext/just_all_the_same/just_all_the_same.c
@@ -5,8 +5,34 @@ VALUE rb_mJustAllTheSame;
 static VALUE
 all_nil_p(VALUE ary)
 {
+  VALUE *forward_p = RARRAY_PTR(ary);
+
+  if (forward_p[0] != Qnil) return Qfalse;
+
+  VALUE *backward_p;
+
+  long size = RARRAY_LEN(ary);
+  long s = size;      // size of foward_ary + backward_aray
+  long v = size / 2;  // splitting index
+  int cmpresult;
+
+  while (v >= 1)
+  {
+    backward_p = forward_p + v;
+
+    /* check last of array nil? if size is not odd? */
+    if (s%2 && forward_p[s-1] != Qnil) return Qfalse;
+
+    /* check quick same back and fowerd */
+    if (memcmp(forward_p, backward_p, sizeof(VALUE) * v)) return Qfalse;
+
+    s = v;
+    v /= 2;
+  }
+
   return Qtrue;
 }
+
 void
 Init_just_all_the_same(void)
 {
