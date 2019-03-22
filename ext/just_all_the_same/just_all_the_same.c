5c776647405dee8fd9c80e92606f4f5dbcbe8702
uproad krowd <7349115+uproad@users.noreply.github.com>
2019-03-23 05:11:46 +0900

    [add] hybrid method.

--- ext/just_all_the_same/just_all_the_same.c
+++ ext/just_all_the_same/just_all_the_same.c
@@ -53,10 +53,52 @@ all_nil_p2(VALUE ary)
   return Qtrue;
 }
 
+static VALUE
+all_nil_p3(VALUE ary)
+{
+  long size = RARRAY_LEN(ary);
+
+  if (size == 0) return Qtrue;
+
+  VALUE *forward_p = RARRAY_PTR(ary);
+
+  if (forward_p[0] != Qnil) return Qfalse;
+
+  VALUE *backward_p;
+
+  long s = size;      // size of foward_ary + backward_aray
+  long v = size / 2;  // splitting index
+
+  while (v >= 1)
+  {
+    backward_p = forward_p + v;
+
+    /* check last of array nil? if size is not odd? */
+    if (s%2 && forward_p[s-1] != Qnil) return Qfalse;
+
+    /* check quick same back and fowerd */
+    if (memcmp(forward_p, backward_p, sizeof(VALUE) * v)) return Qfalse;
+
+    s = v;
+    if (s <= 800) break;
+    v /= 2;
+  }
+
+  long i = 1;
+  while (i < s)
+  {
+    if (forward_p[i] != Qnil) return Qfalse;
+    i++;
+  }
+
+  return Qtrue;
+}
+
 
 void
 Init_just_all_the_same(void)
 {
   rb_define_method(rb_cArray, "all_nil?", all_nil_p, 0);
   rb_define_method(rb_cArray, "all_nil2?", all_nil_p2, 0);
+  rb_define_method(rb_cArray, "all_nil3?", all_nil_p3, 0);
 }
